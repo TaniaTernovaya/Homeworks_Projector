91fafdd7dacc24a1f6cf1dfce5ceddceb33d2a79
Tania Ternovaya <ternovaya@live.com>
2023-07-19 00:12:35 +0300

    Homework_8

---
+++ Homework_8/cats.py
@@ -0,0 +1,16 @@
+def cats(number_cats:list, number_round) -> list:
+    cats = [1 for i in range(number_cats)]
+    result = []
+    for i in range(2,number_round+1):
+        for number,cat in enumerate(cats):
+            if ((number+1) % i == 0) and (cat == 0):
+                cats[number] = 1
+            if ((number+1) % i == 0) and (cat == 1):
+                cats[number] = 0
+    for number,hat in enumerate(cats,start=1):
+        if hat == 1:
+            result.append(number)
+    return result
+
+
+print(cats(1000,1000))
